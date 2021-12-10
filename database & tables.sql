0bd620bacd2d3379401738566c4de19532ab1223
Saeed-as <52257884+Saeed-as@users.noreply.github.com>
2021-12-10 23:19:17 +0300

    Uplaod

--- database & tables.sql
+++ database & tables.sql
@@ -26,7 +26,7 @@ CREATE DATABASE IF NOT EXISTS `seu_saeed_874413` CHARACTER SET utf8 COLLATE utf8
 -- Table structure for table `employee`
 --
 
-CREATE TABLE `employee` (
+CREATE TABLE `seu_saeed_874413`.`employee` (
   `id` int(11) NOT NULL,
   `employee_no` varchar(20) DEFAULT NULL,
   `employee_name_en` varchar(100) NOT NULL,
@@ -44,7 +44,7 @@ CREATE TABLE `employee` (
 -- Dumping data for table `employee`
 --
 
-INSERT INTO `employee` (`id`, `employee_no`, `employee_name_en`, `employee_name_ar`, `employee_id`, `birth_date`, `nationality`, `mobile_no`, `active`, `create_by`, `create_at`) VALUES
+INSERT INTO `seu_saeed_874413`.`employee` (`id`, `employee_no`, `employee_name_en`, `employee_name_ar`, `employee_id`, `birth_date`, `nationality`, `mobile_no`, `active`, `create_by`, `create_at`) VALUES
 (16, '123456', 'Saeed Ali Saeed', 'سعيد علي سعيد', 1234567890, '1980-10-01', 35, 511223344, 1, 1, '2021-12-10 16:22:00'),
 (17, '123457', 'Mohammed Ahmed', 'محمد احمد', 1234567880, '1975-01-01', 37, 577998841, 1, 1, '2021-12-10 16:23:00'),
 (18, '123458', 'Khalid Ibrahim', 'خالد ابراهيم', 1234567870, '1990-01-01', 40, 599887741, 1, 1, '2021-12-10 16:24:00'),
@@ -56,7 +56,7 @@ INSERT INTO `employee` (`id`, `employee_no`, `employee_name_en`, `employee_name_
 -- Table structure for table `employee_nationality`
 --
 
-CREATE TABLE `employee_nationality` (
+CREATE TABLE `seu_saeed_874413`.`employee_nationality` (
   `id` int(11) NOT NULL,
   `nationality` varchar(50) NOT NULL,
   `active` tinyint(1) NOT NULL DEFAULT 1,
@@ -68,7 +68,7 @@ CREATE TABLE `employee_nationality` (
 -- Dumping data for table `employee_nationality`
 --
 
-INSERT INTO `employee_nationality` (`id`, `nationality`, `active`, `create_by`, `create_at`) VALUES
+INSERT INTO `seu_saeed_874413`.`employee_nationality` (`id`, `nationality`, `active`, `create_by`, `create_at`) VALUES
 (35, 'Saudi', 1, 1, '2021-12-10 16:09:00'),
 (36, 'Hindi', 0, 1, '2021-12-10 16:09:00'),
 (37, 'Egyptian', 1, 1, '2021-12-10 16:14:00'),
@@ -85,13 +85,13 @@ INSERT INTO `employee_nationality` (`id`, `nationality`, `active`, `create_by`, 
 --
 -- Indexes for table `employee`
 --
-ALTER TABLE `employee`
+ALTER TABLE `seu_saeed_874413`. `employee`
   ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `employee_nationality`
 --
-ALTER TABLE `employee_nationality`
+ALTER TABLE `seu_saeed_874413`. `employee_nationality`
   ADD PRIMARY KEY (`id`);
 
 --
@@ -101,13 +101,13 @@ ALTER TABLE `employee_nationality`
 --
 -- AUTO_INCREMENT for table `employee`
 --
-ALTER TABLE `employee`
+ALTER TABLE `seu_saeed_874413`. `employee`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
 
 --
 -- AUTO_INCREMENT for table `employee_nationality`
 --
-ALTER TABLE `employee_nationality`
+ALTER TABLE `seu_saeed_874413`. `employee_nationality`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=43;
 COMMIT;
 
